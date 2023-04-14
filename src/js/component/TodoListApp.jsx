ce5a7acc995f1c52e92f5202236a492e7f90c266
joaarg86 <johanna.e.escobar@icloud.com>
2023-04-14 05:08:53 +0000

    johanna

--- src/js/component/TodoListApp.jsx
+++ src/js/component/TodoListApp.jsx
@@ -7,20 +7,29 @@ function TodoListApp() {
   //   // job:"Teacher"
   // };
   const [tasks, setTasks] = useState([]);
-  const [inputText, setInputText] = useState("");
+  const [inputText, setInputText] = useState(" ");
   const [items, setItems] = useState([]);
   const [erase, setErase] = useState();
-  const [newUser, setnewUser] = useState();
+  //const [newUser, setnewUser] = useState();
+
+  ///////////////////////Miguel////////////////////////////
+
+  const [user, setUser] = useState("");
+  const [createUserSucces, setCreateUserSucces] = useState(false);
+  const [deleteUserSucces, setDeleteUserSucces] = useState(false);
+  const [sentUser, setSentUser] = useState(false);
+  const [sentDelete, setSentDelete] = useState(false);
+  /////////////////////////////////////////////////////////
 
   //GET---
 
-  useEffect(()=>{
-      fetch("https://assets.breatheco.de/apis/fake/todos/user/teamgeekuser")
-      .then (response=>response.json())
-      .then (data=>{
-          setTasks(data);
+  useEffect(() => {
+    fetch("https://assets.breatheco.de/apis/fake/todos/user/teamgeekuser")
+      .then((response) => response.json())
+      .then((data) => {
+        setTasks(data);
       });
-  },[]);
+  }, []);
 
   //POST-----------------------------------
   const addTask = (e) => {
@@ -30,7 +39,7 @@ function TodoListApp() {
     fetch("https://assets.breatheco.de/apis/fake/todos/user/teamgeekuser", {
       method: "POST",
       headers: {
-        "Content-Type": "applocation/json",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         task: inputTask.value,
@@ -40,7 +49,7 @@ function TodoListApp() {
       .then((data) => {
         const newTasks = [...tasks, data];
         setTasks(newTasks);
-        inputTask.value = "";
+        inputTask.value = " ";
       });
   };
 
@@ -118,7 +127,6 @@ function TodoListApp() {
 
   //DELETE-----------------------------------------------------------------------
 
-
   useEffect(() => {
     if (erase) {
       let newArray = items.filter((element) => element.id != erase);
@@ -128,29 +136,29 @@ function TodoListApp() {
 
   //POST ADD NEWUSER-----------------------------------------------------------------
 
-  const handleOpenUser = () => {
-    fetch("https://assets.breatheco.de/apis/fake/todos/user/${newUser}")
-      .then((res) => {
-        if (!res.ok) {
-          return fetch(
-            "https://assets.breatheco.de/apis/fake/todos/user/${newUser}",
-            {
-              method: "POST",
-              body: JSON.stringify(newUser),
-              headers: {
-                "Content-Type": "application/json",
-              },
-            }
-          [setnewUser],)
-        }
-        return res;
-      })
-      .then((res) => res.json())
-      .then((data) => setItems(data))
-      .catch((error) => {
-        console.log("error");
-      });
-  };
+  // const handleOpenUser = () => {
+  //   fetch("https://assets.breatheco.de/apis/fake/todos/user/${newUser}")
+  //     .then((res) => {
+  //       if (!res.ok) {
+  //         return fetch(
+  //           "https://assets.breatheco.de/apis/fake/todos/user/${newUser}",
+  //           {
+  //             method: "POST",
+  //             body: JSON.stringify(newUser),
+  //             headers: {
+  //               "Content-Type": "application/json",
+  //             },
+  //           }[setnewUser]
+  //         );
+  //       }
+  //       return res;
+  //     })
+  //     .then((res) => res.json())
+  //     .then((data) => setItems(data))
+  //     .catch((error) => {
+  //       console.log("error");
+  //     });
+  // };
 
   const updateText = (e) => {
     setInputText(e.target.value);
@@ -160,7 +168,7 @@ function TodoListApp() {
     if (inputText.trim() == " ") return;
     const newItem = {
       text: `${inputText}`,
-      // label: "Add a Task",
+      label: "Add a Task",
       done: false,
       id: (Math.random() * 20).toFixed(1),
     };
@@ -169,10 +177,51 @@ function TodoListApp() {
     console.log(items);
   };
 
+  const createUser = async (newUser) => {
+    setSentUser(true);
+    let userResponse = await fetch(
+      `https://assets.breatheco.de/apis/fake/todos/user/${newUser}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([]),
+      }
+    );
+
+    if (userResponse.ok) {
+      setCreateUserSucces(true);
+    } else {
+      setCreateUserSucces(false);
+    }
+  };
+
+  const deleteUser = async (newUser) => {
+    setSentDelete(true);
+
+    let deleteResponse = await fetch(
+      `https://assets.breatheco.de/apis/fake/todos/user/${newUser}`,
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify([]),
+      }
+    );
+
+    if (deleteResponse.ok) {
+      setDeleteUserSucces(true);
+    } else {
+      setDeleteUserSucces(false);
+    }
+  };
+
   return (
     <div>
       <nav>
-        <button className="home-button btn btn-secondary">Some button</button>
+        {/* <button className="home-button btn btn-secondary">Some button</button> */}
       </nav>
       <main>
         <h1>To Do List</h1>
@@ -180,25 +229,84 @@ function TodoListApp() {
           <input
             className="app-input"
             onChange={updateText}
-            value={inputText}
             type="text"
             id="task-input"
             placeholder="No tasks, Add a task!"
           />
+
           <button
             className="app-submit"
             onClick={handleNewTask}
-            //title="Add task"
+            title="Add task"
           >
             <i className="fas fa-plus" />
           </button>
         </form>
         {/* <Home listOfTasks={items} onDelete={setErase} /> */}
-        <input className="app-input" placeholder="New User..." />
+        {/* <input className="app-input" placeholder="New User..." />
         <button className="btn btn-danger" onClick={handleOpenUser}>
           Load User
+        </button> */}
+
+        <input
+          className="app-input"
+          placeholder="New User..."
+          onChange={(e) => {
+            setUser(e.target.value);
+          }}
+        />
+        <div>{"user:" + user}</div>
+
+        <button
+          className="btn btn-danger"
+          onClick={(e) => {
+            createUser(user);
+          }}
+        >
+          Create User
         </button>
+
+        {sentUser && (
+          <div>
+            {createUserSucces ? (
+              <div className="alert alert-success" role="alert">
+                User Succesfully Created!
+              </div>
+            ) : (
+              <div className="alert alert-danger" role="alert">
+                User not created! User Name Already in Use!
+              </div>
+            )}
+          </div>
+        )}
+
+        <br />
+
+        <button
+          className="btn btn-danger mt-1"
+          onClick={(e) => {
+            deleteUser(user);
+          }}
+        >
+          Delete User
+        </button>
+
+        {sentDelete && (
+          <div>
+            {deleteUserSucces ? (
+              <div className="alert alert-success" role="alert">
+                User deleted successfully!
+              </div>
+            ) : (
+              <div className="alert alert-danger" role="alert">
+                User not deleted or Unexsistant User Id (User has already been
+                deleted!)
+              </div>
+            )}
+          </div>
+        )}
       </main>
+      <br />
       <footer>
         <p>Made with ❤️ by 4Geeks Team</p>
       </footer>
